b887822e804bfe4c81d93ef715c17f8da5e6204b
nmansor <nasser.owen@gmail.com>
2019-11-13 19:30:03 -0500

    Updated UserRegistrationNM

--- Models/UserRegistrationVM.cs
+++ Models/UserRegistrationVM.cs
@@ -9,6 +9,7 @@ namespace WebApi.Models
     {
         public string Email { get; set; }
         public string FirstName { get; set; }
+        public string MiddleName { get; set; }
         public string FamilyName { get; set; }
         public string Password { get; set; }
     }
